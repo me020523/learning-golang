b160373e3c630d2817210a843427b5daafc69cec
bcshuai <bcshuai@cn.ibm.com>
2016-10-20 09:41:14 +0800

    demo code with interface

--- type-test.go
+++ type-test.go
@@ -4,9 +4,21 @@ import (
 	"fmt"
 )
 
-func main() {
+func testSlice() {
 	tp := new([7]string)
 	sliceTp := tp[:]
 	fmt.Printf("tp is the type of %T\n", *tp)
 	fmt.Printf("sliceTp is the type of %T\n", sliceTp)
+}
+func testNilAndInterface(i I) {
+	i.display()
+}
+
+type I interface {
+	display()
+}
+
+func main() {
+	var i I = nil
+	testNilAndInterface(i)
 }
